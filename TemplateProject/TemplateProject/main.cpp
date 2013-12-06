d1395c29f823449e07902ea350ffac36bfed26ea
Daniel Henderson <tindrum@mac.com>
2013-12-05 18:10:07 -0800

    first commit

---
+++ TemplateProject/TemplateProject/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  TemplateProject
+//
+//
+//  Created by Daniel Henderson on 12/5/13.
+//  Copyright (c) 2013 ___Dan___. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+
+    return 0;
+}
